81b6673935d99620c5e1e9449ac149a9a8099598
mzh <manzhenhua@sunlands.com>
2018-11-20 20:31:13 +0800

    配置eslint

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from '@/components/head';
-
+const ss = 123;
 class App extends Component {
   render() {
-    console.log(process.env)
+    console.log(process.env);
     return (
       <div className="App">
         <header className="App-header">
-        <Header/>
+          <Header />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
